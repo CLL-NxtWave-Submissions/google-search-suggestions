798300c83ceafbbf3df4651c636609f48d8fce11
Surya T N <suryatej.nim@yahoo.com>
2022-09-04 06:42:40 +0000

    Implemented SuggestionItem component UI with element layout and event handlers in the corresponding index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,1 +1,27 @@
-// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {searchSuggestion, searchSuggestionSelectionHandler} = props
+
+  const onSearchSuggestionSelectionEvent = () =>
+    searchSuggestionSelectionHandler(searchSuggestion)
+
+  return (
+    <li className="suggestion-item-container">
+      <p className="suggestion-text">{searchSuggestion}</p>
+      <button
+        type="button"
+        className="suggestion-selection-button"
+        onClick={onSearchSuggestionSelectionEvent}
+      >
+        <img
+          className="selection-arrow-img"
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
